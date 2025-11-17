1a24fef27a30fca7d97d0e14a247d06ee8c9343a
QuanT4_HoangMai <davidnguyen67dev@gmail.com>
2025-11-17 08:40:02 +0700

    Add initial project files including HTML, CSS, JS, images, and audio assets for interactive web experience

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+  titleWeb: 'Êi Ngọc Ánh, lại đây tớ hỏi cái 😏',
+  introTitle: 'Ngọc Ánh – bạn thỏ xinh nhất hệ mặt trời 🐰',
+  introDesc: `Không biết sao thỏ gì mà nhìn phát là thấy vui rồi 😌  
+  kiểu xuất hiện cái là mood lên luôn á.`,
+  btnIntro: 'Bấm thử coi 👀',
+  title: 'Người ta bảo thỏ thì nhút nhát...',
+  desc: 'Nhưng bạn thỏ này coi vậy chứ quậy lắm 😜',
+  btnYes: 'Ờ tớ biết mà 😎',
+  btnNo: 'Cậu nói linh tinh á 😤',
+  question: 'Nếu ai gọi cậu là “bạn thỏ cute”, cậu thấy sao? 🤔',
+  btnReply: 'Tớ trả lời nè 😆',
+  reply: 'Ổn chứ, miễn là người gọi không xàm 🤣',
+  mess: 'Hehe biết ngay luôn 😏 Thỏ này biết gây nghiện ghê 😂',
+  messDesc: 'Đi uống trà sữa hem? Tớ bao luôn topping 🧋',
+  btnAccept: 'Lẹt gô 😄',
+  messLink: 'https://www.messenger.com/t/aihihi13/',
+};
